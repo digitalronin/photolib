bd751b42a429b1f338db0ce0042837d6b74c3ab7
digitalronin <david@digitalronin.com>
2024-03-16 15:40:44 +0900

    WiP: Add a stub Gallery component

---
+++ frontend/src/PageGallery.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Gallery from './components/Gallery';
+
+const PageGallery = () => {
+  return (
+    <div>
+      <Gallery />
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    </div>
+  )
+}
+
+export default PageGallery
---
+++ frontend/src/components/Gallery.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {API_SERVER} from '../config'
+
+const Gallery = () => {
+  return (
+    <div className="container mt-3" >
+      <div className="row mt-3">
+        <h1>Gallery</h1>
+      </div>
+    </div >
+  )
+}
+
+export default Gallery
