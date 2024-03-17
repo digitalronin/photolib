4c4af8f232c77b89d4a03f9050f53b097bc1c01e
digitalronin <david@digitalronin.com>
2024-03-17 13:44:37 +0900

    Use react-image-gallery

--- frontend/src/components/Gallery.js
+++ frontend/src/components/Gallery.js
@@ -1,57 +1,42 @@
 import React, {useEffect, useState} from 'react'
 import {API_SERVER} from '../config'
-import DisplayImage from "./DisplayImage"
-import ItemThumbnail from "./ItemThumbnail"
 import {stringToHex} from '../utils'
+import ImageGallery from "react-image-gallery"
+import "react-image-gallery/styles/css/image-gallery.css"
 
 const Gallery = () => {
-  const GALLERY_VIEW = 1
-  const DISPLAY_VIEW = 2
-
-  const [viewMode, setViewMode] = useState(GALLERY_VIEW)
   const [mediaItems, setMediaItems] = useState([])
-  const [currentItemIndex, setCurrentItemIndex] = useState(0)
 
   useEffect(() => {fetchMedia()}, [])
+
+  const getUrl = item => {
+    const hex = stringToHex(item.filepath)
+    return `${API_SERVER}/image/${hex}`
+  }
 
   const fetchMedia = async () => {
     let url = `${API_SERVER}/api/images`
     try {
       const response = await fetch(url)
       const data = await response.json()
-      const indexedItems = data.items.map((obj, index) => { return { ...obj, index }
-});
+      const indexedItems = data.items.map((item, index) => {
+        return {
+          ...item,
+          index,
+          original: getUrl(item),
+          thumbnail: getUrl(item),
+        }
+      })
       setMediaItems(indexedItems)
     } catch (error) {
       console.error('Error fetching media items:', error)
     }
   }
 
-  const clickThumbnail = index => {
-    console.log(`Set currentItemIndex to ${index}`)
-    setCurrentItemIndex(index)
-    setViewMode(DISPLAY_VIEW)
-  }
-
-  let imageComponent
-
-  if (viewMode === GALLERY_VIEW) {
-    imageComponent = <div className="row mt-3" style={{display: "inline"}}>
-      {mediaItems.map(item => (<ItemThumbnail key={item.index} item={item} onClick={() => clickThumbnail(item.index) } />))}
-    </div>
-  }
-
-  if (viewMode === DISPLAY_VIEW) {
-    const item = mediaItems[currentItemIndex]
-    const hex = stringToHex(item.filepath)
-    imageComponent = <DisplayImage hex={hex} returnToGallery={() => setViewMode(GALLERY_VIEW)}/>
-  }
-
   return (
-    <div className="container mt-3" >
-      {imageComponent}
-    </div >
+    <ImageGallery items={mediaItems} />
   )
+
 }
 
 export default Gallery
